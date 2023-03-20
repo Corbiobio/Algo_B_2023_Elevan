c7f8635645e75c7abce5cdd483cc92d7c649ebaf
corbiobio <edarnand@edenschool.fr>
2023-03-20 13:51:39 +0100

    exo 3

--- app.js
+++ app.js
@@ -12,14 +12,24 @@
 
 //exo 2
 
-function UnaDix() {
+// function UnaDix() {
 
-    let num = ""
-    for (let i = 1; i <= 10; i++) {
-        num += i + " "
-        console.log(num);
+//     let num = ""
+//     for (let i = 1; i <= 10; i++) {
+//         num += i + " "
+//         console.log(num);
+//     }
+// }
+// console.log(UnaDix());
+
+
+// exo 3 
+
+function impaires() {
+    let num_impaires = ""
+    for (let i = 1; i < 100; i += 2) {
+        num_impaires += i + " "
+        console.log(num_impaires);
     }
 }
-console.log(UnaDix());
-
-
+console.log(impaires());
