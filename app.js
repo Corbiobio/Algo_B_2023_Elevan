191f024fc40b26ef2e4429261ddeab44e51db75f
corbiobio <edarnand@edenschool.fr>
2023-03-20 13:44:53 +0100

    exo 2

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // exo 1 
 
+// function UnaDix() {
+
+//     let num = ""
+//     for (let i = 1; i <= 10; i++) {
+//         num += i
+//         console.log(num);
+//     }
+// }
+// console.log(UnaDix());
+
+//exo 2
+
 function UnaDix() {
 
     let num = ""
     for (let i = 1; i <= 10; i++) {
-        num += i
+        num += i + " "
         console.log(num);
     }
 }
